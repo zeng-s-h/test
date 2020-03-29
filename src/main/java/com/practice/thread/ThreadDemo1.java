b544221ef41c6117b7b8bc2818559ddc61443037
PSYCHO129 <18086450050@163.com>
2020-03-29 12:56:38 +0800

    测试提交

--- src/main/java/com/practice/thread/ThreadDemo1.java
+++ src/main/java/com/practice/thread/ThreadDemo1.java
@@ -8,14 +8,17 @@ import com.practice.thread.entity.MyThread;
  */
 public class ThreadDemo1 {
 
+    /**
+     * 测试
+     * @param args 参数
+     * @throws InterruptedException 异常信息
+     */
     public static void main(String[] args) throws InterruptedException {
         Thread s1 = new Thread(new MyThread("111",5000));
         Thread s2 = new Thread(new MyThread("222",1000));
         Thread s3 = new Thread(new MyThread("333",3000));
 
         s1.start();
-
-
 //        s1.join();
 
         s2.start();
